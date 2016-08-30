25eec9dba13d0882f5358e6a04715bb65f694ac7
Karina Denisevich <karina_41997@mail.ru>
2016-08-30 16:17:05 +0300

    master: check for math pow exceptions implemented

--- src/main/java/com/epam/calculator/mathematics/Exponentiation.java
+++ src/main/java/com/epam/calculator/mathematics/Exponentiation.java
@@ -3,7 +3,14 @@ package com.epam.calculator.mathematics;
 public class Exponentiation implements Mathematics {
 
     @Override
-    public Double compute(double a, double b) {
+    public Double compute(double a, double b) throws ArithmeticException {
+
+        if (a < 0 && b % 1 != 0) {
+            throw new ArithmeticException("Extracting of root of a negative number!");
+        } else if (a == 0 && b < 0) {
+            throw new ArithmeticException("Division by zero!");
+        }
+
         return Math.pow(a, b);
     }
 }
